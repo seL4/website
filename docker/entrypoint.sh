e1d89b3e9dd039a28cfd5b63e0d1025b321bd8f6
Gerwin Klein <gerwin.klein@proofcraft.systems>
2023-12-22 12:43:02 +0700

    docker: update docker setup for jekyll
    The docker image can generate the site and serve a preview, but the link
    checker functionality is not updated yet.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 #
-# Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright 2023, Proofcraft Pty Ltd
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
 
 set -ex
 
-bash docker/launcher.sh
+cd /mnt
+bundle install
+
+if [ $# -eq 0 ]; then
+    make build
+else
+    make "$@"
+fi
