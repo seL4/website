f8cf416d38b4c1c223e540ca854e6f8e531b66a3
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-07-21 16:49:44 +1000

    front-page: logo scroll
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,15 @@ module.exports = {
       },
       colors: {
         logogreen: '#96ca4f'
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
       }
     },
   },
