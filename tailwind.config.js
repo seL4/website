516c081d7d43925e54519f0707e6b0916305d919
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-08-11 16:19:40 +0200

    css: refactor colours; make utility layer
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   content: [
@@ -13,7 +14,15 @@ module.exports = {
         'sans': ['Roboto', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        logogreen: '#96ca4f'
+        darkcol: colors.gray['900'],
+        darkcol_inv: colors.gray['300'],
+        lightcol: colors.gray['600'],
+        lightcol_inv: colors.gray['400'],
+        logogreen: '#96ca4f',
+        darkblue: '#030020',
+        accentlight: colors.indigo['400'],
+        accent: colors.indigo['500'],
+        accentdark: colors.indigo['600']
       },
       animation: {
         'infinite-scroll': 'infinite-scroll 50s linear infinite',
