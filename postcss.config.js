9aef10c4e8ef3deceed4a8d506299f2a41db08ce
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-01-24 14:28:52 +1100

    tailwind: upgrade to v4
    - colour and font theme now in _includes/theme.css
    - small adjustments in html pages for new names/syntax
    - tailwind config moved to sel4.css
    - postcss config simplified (autoprefixer now included in tailwind)
    - switch to jekyll-postcss-v2, because jekyll-postcss no longer works
      (also nicer error reporting)
    - npm and ruby package upgrades
    - re-add live-reload to Makefile, no longer seems to steal focus
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    // ...(process.env.JEKYLL_ENV == 'production'
-    //   ? [require('cssnano')({ preset: 'default' })]
-    //   : [])
+    require('@tailwindcss/postcss'),
   ]
 }
